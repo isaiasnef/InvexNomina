4cb5198edc11533dad8801a8c40839505be74651
Neftali Torres Angel <Neftali@MacBook-de-Neftali.local>
2020-06-17 12:47:59 -0500

    Primera versión de la librería ligeun (Librería General de Utilerías de Neftali).

---
+++ src/main/sources/com/neftali/ligeun/App.java
@@ -0,0 +1,13 @@
+package com.neftali.ligeun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/sources/com/neftali/ligeun/AppSpringConfig.java
@@ -0,0 +1,14 @@
+package com.neftali.ligeun;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Base configuration for spring
+ */
+@Configuration
+@ComponentScan("com.neftali.ligeun")
+public class AppSpringConfig
+{
+}
